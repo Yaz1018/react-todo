5129b3ef29d06e28685850247e3cfa9032d8ee77
Yaz1018 <garoncdavis@gmail.com>
2017-02-19 21:08:03 -0500

    added created and completed at fields

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require("react");
 var uuid = require('uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -24,7 +25,9 @@ var TodoApp = React.createClass({
                 {
                     id: uuid(),
                     text: todo,
-                    completed: false
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
                 }
             ]
         });
@@ -39,6 +42,7 @@ var TodoApp = React.createClass({
         var updatedTodos = this.state.todos.map(function (todo) {
             if (id === todo.id) {
                 todo.completed = !todo.completed;
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
             }
             return todo;
         })
--- playground/moment-examples.js
+++ playground/moment-examples.js
@@ -1,2 +1,13 @@
 var moment = require('moment');
 
+console.log(moment().format());
+
+var now = moment();
+
+console.log('Current timestamp', now.unix());
+
+var timestamp = 1487554725;
+
+var currentMoment = moment.unix(timestamp);
+
+console.log('current moment', currentMoment.format('MMM D, YY'))
