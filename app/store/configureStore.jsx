b14e448593d9586a0f8aa36f21dea44c180520ab
Garon <garoncdavis@gmail.com>
2017-02-22 10:19:00 -0500

    add redux store

---
+++ app/store/configureStore.jsx
@@ -0,0 +1,20 @@
+var redux = require('redux');
+var thunk = require('redux-thunk').default;
+
+
+var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
+
+export var configure = () => {
+    var reducer = redux.combineReducers({
+        searchText: searchTextReducer,
+        showCompleted: showCompletedReducer,
+        todos: todosReducer
+    });
+
+    var store = redux.createStore(reducer, redux.compose(
+        redux.applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ));
+
+    return store;
+};
